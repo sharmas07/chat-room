3c7dbff42907f966e029c265ace2ee914ae3f911
sharmas07 <valis6435@gmail.com>
2024-02-14 18:49:44 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className="app__wrapper bg-black">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-pink-400 w-10 h-10'>
+        <h2>Quick Chat</h2>
+    </div>
+  )
+}
+
+export default Navbar
